7073a963ccc94a180e9e0f6db4c920d088f12eb6
nehapantGL <kohl@administrators-Mac-mini.local>
2014-01-09 09:50:50 +0530

    check

---
+++ HelloWorld/Check/Check/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Check
+//
+//  Created by Kohl on 1/9/14.
+//  Copyright (c) 2014 Kohl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/Check/Check/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Check
+//
+//  Created by Kohl on 1/9/14.
+//  Copyright (c) 2014 Kohl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
